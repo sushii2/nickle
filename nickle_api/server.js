58237875cbaeb9098a8853d8a64571df8417a6a5
Saksham Saraswat <sushi@Sakshams-MacBook-Air.local>
2020-02-28 18:40:18 -0600

    initialize repository

--- nickle_api/server.js
+++ nickle_api/server.js
@@ -5,6 +5,15 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.status(200);
+    res.send('Server initialized');
+})
+
+app.get('/api/v1/projects', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Show all project cars' });
+})
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
