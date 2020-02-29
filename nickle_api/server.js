7495b451d9c2a5a391756914d7cb2c9ac9e423ff
Saksham Saraswat <sushi@Sakshams-MacBook-Air.local>
2020-02-28 18:58:37 -0600

    Created sample CRUD routes for project car

--- nickle_api/server.js
+++ nickle_api/server.js
@@ -8,11 +8,27 @@ const app = express();
 app.get('/', (req, res) => {
     res.status(200);
     res.send('Server initialized');
-})
+});
 
 app.get('/api/v1/projects', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Show all project cars' });
-})
+  res.status(200).json({ success: true, msg: 'Get all project cars' });
+});
+
+app.get('/api/v1/projects/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `Get project car ${req.params.id}` });
+});
+
+app.post('/api/v1/projects', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Create new project car' });
+});
+
+app.put('/api/v1/projects/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `Display project car ${req.params.id}` });
+});
+
+app.delete('/api/v1/projects/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `Delete project car ${req.params.id}` });
+});
 
 const PORT = process.env.PORT || 5000;
 
