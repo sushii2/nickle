551efe3e0ac88537a9ca23ea656f377c6a5e0fde
sushii2 <sakshamsaraswat0208@gmail.com>
2020-03-15 17:28:35 -0500

    Added a middleware to get location data using MapQuest API

--- nickle_api/models/Project.js
+++ nickle_api/models/Project.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
 
 const ProjectSchema = new mongoose.Schema({
     name: {
@@ -154,8 +156,7 @@ const ProjectSchema = new mongoose.Schema({
         ]
     },
     address: {
-        type: String,
-        required: [true, 'Please add an address.']
+        type: String
     },
     location: {
         type: {
@@ -185,6 +186,31 @@ const ProjectSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     }
+});
+
+// Create project slug from the name.
+ProjectSchema.pre('save', function(next) {
+    this.slug = slugify(this.name, { lower:true });
+    next();
+});
+
+// Geocode & create location field
+ProjectSchema.pre('save', async function(next) {
+   const loc =  await geocoder.geocode(this.address);
+   this.location = {
+       type: 'Point',
+       coordinates: [loc[0].longitude, loc[0].latitude],
+       formattedAddress: loc[0].formattedAddress,
+       street: loc[0].streetName,
+       city: loc[0].city,
+       state: loc[0].stateCode,
+       zipcode: loc[0].zipcode,
+       country: loc[0].countryCode
+   }
+
+   // Do not save address in DB
+   this.address = undefined;
+    next();
 });
 
 module.exports = mongoose.model('Project', ProjectSchema);
