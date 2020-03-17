15994f6a70e55e1a574ead69ce37034c6f2dc8e0
sushii2 <sakshamsaraswat0208@gmail.com>
2020-03-16 19:12:31 -0500

    Created post model

---
+++ nickle_api/models/Post.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        trim: true,
+        require: [true, 'Please add a post title.']
+    },
+    description: {
+        type: String,
+        required: [true, 'Please add a description']
+    },
+    photoUrl: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    project: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Project',
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Post', PostSchema);
+
