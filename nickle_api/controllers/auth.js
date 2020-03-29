f58428690f4971bde6a522781c63b9ab4d4ded54
sushii2 <sakshamsaraswat0208@gmail.com>
2020-03-28 19:13:45 -0500

    Created register auth and added bcrypt middleware

--- nickle_api/controllers/auth.js
+++ nickle_api/controllers/auth.js
@@ -6,5 +6,16 @@ const User = require("../models/User");
 //@route   GET /api/v1/auth/register
 //@access  Public
 exports.register = asyncHandler(async (req, res, next) => {
-    res.status.json({ success: true });
+    const { name, username, email, password, role } = req.body;
+
+    // Create user
+    const user = await User.create({
+        name,
+        username,
+        email,
+        password,
+        role
+    });
+
+    res.status(200).json({ success: true });
 });
--- nickle_api/models/User.js
+++ nickle_api/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -37,5 +38,12 @@ const UserSchema = new mongoose.Schema({
         default: Date.now
     }
 });
+
+// Encrypt password using bcrypt
+UserSchema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password);
+})
+
 
 module.exports = mongoose.model('User', UserSchema);
