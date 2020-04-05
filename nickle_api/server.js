df27745ff9610fda52ff7624603bc2c0e1237f10
sushii2 <sakshamsaraswat0208@gmail.com>
2020-04-04 19:41:36 -0500

    Added security dependencies

--- nickle_api/server.js
+++ nickle_api/server.js
@@ -2,9 +2,15 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const colors = require("colors");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require('cors');
 const errorHandler = require("./middleware/error");
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
 
 dotenv.config({ path: "./config/config.env" });
 
@@ -12,10 +18,10 @@ dotenv.config({ path: "./config/config.env" });
 connectDB();
 
 //Route files
-const projects = require('./routes/projects');
-const posts = require('./routes/posts');
-const auth = require('./routes/auth');
-const reviews = require('./routes/reviews');
+const projects = require("./routes/projects");
+const posts = require("./routes/posts");
+const auth = require("./routes/auth");
+const reviews = require("./routes/reviews");
 
 const app = express();
 
@@ -26,15 +32,37 @@ app.use(express.json());
 app.use(cookieParser());
 
 //Dev logging middleware
-if(process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 100
+});
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
+
 //Mount routers
-app.use('/api/v1/projects', projects);
-app.use('/api/v1/posts', posts);
-app.use('/api/v1/auth', auth);
-app.use('/api/v1/reviews', reviews);
+app.use("/api/v1/projects", projects);
+app.use("/api/v1/posts", posts);
+app.use("/api/v1/auth", auth);
+app.use("/api/v1/reviews", reviews);
 
 // Custom express error handler
 app.use(errorHandler);
@@ -43,11 +71,13 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
 
 // Handle unhandled promise rejections
-process.on('unhandledRejection', (err, promise) => {
+process.on("unhandledRejection", (err, promise) => {
   console.log(`Error: ${err.message}`.red);
   //Close server & exit process
   server.close(() => process.exit(1));
